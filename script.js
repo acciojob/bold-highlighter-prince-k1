9df7fb03edc6ed28bdb2c44244837b9068c79662
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-26 20:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+for(i of bold){
+	i.style.color = 'green';
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+for(i of bold){
+		i.style.color = 'black';
+	  }
     
 }
+const bold = document.getElementsByTagName('strong');
+
